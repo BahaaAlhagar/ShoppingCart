888dadbebaa3b1935cf49cd9fa94ae8dc57dd1fb
BahaaAlhagar <damrawyy@yahoo.com>
2017-12-31 13:06:42 +0200

    recude one item function

--- src/ShoppingCart.php
+++ src/ShoppingCart.php
@@ -27,15 +27,31 @@ class ShoppingCart
         
     }
 
+
     /**
-     * Friendly welcome
+     * update the cart in the Session
+     *
+     * @return update the session cart array and the cart object 
+     */
+    public function update()
+    {
+        // add the Cart to the Session
+        Session::put('cart', $this);
+
+        // return the Cart if needed
+        return $this;
+    }
+
+    /**
+     * Add 1 item to the Cart
      *
      * @param object $item item to add
      *
-     * @return string Returns the phrase passed in
+     * @return update the session cart array and the cart object 
      */
     public function add($item)
     {
+        // we need id for the array index
         $id = $item->id;
 
         // add the item properties
@@ -58,10 +74,36 @@ class ShoppingCart
         $this->totalQty++;
         $this->totalPrice += $item->price;
 
-        // add the Cart to the Session
-        Session::put('cart', $this);
+        // add the cart to the Session
+        $this->update();
+    }
 
-        // return the Cart if needed
-        return $this;
+    /**
+     * Reduce 1 item from the Cart
+     *
+     * @param object $item item to reduce by 1
+     *
+     * @return update the session cart array and the cart object 
+     */
+    public function reduceOneItem($item)
+    {
+        // we need id for the array index
+        $id = $item->id;
+
+        // decrease the qty and the price in the cart
+        $this->items[$id]['qty']--;
+        $this->items[$id]['price'] -= $this->items[$id]['item']['price'];
+        $this->totalQty--;
+        $this->totalPrice -= $this->items[$id]['item']['price'];
+
+        // if the qty is 0 or less remove the item from Cart
+        if($this->items[$id]['qty'] <= 0)
+        {
+            unset($this->items[$id]);
+        }
+
+        // add the cart to the Session
+        $this->update();
     }
 }
+
