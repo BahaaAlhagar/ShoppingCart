9a28eaae466efe892ba460a1b24c65f1273a3018
BahaaAlhagar <damrawyy@yahoo.com>
2017-12-31 12:51:50 +0200

    Shopping Cart add item function

--- src/ShoppingCart.php
+++ src/ShoppingCart.php
@@ -30,12 +30,38 @@ class ShoppingCart
     /**
      * Friendly welcome
      *
-     * @param string $phrase Phrase to return
+     * @param object $item item to add
      *
      * @return string Returns the phrase passed in
      */
-    public function echoPhrase($phrase)
+    public function add($item)
     {
-        return $phrase;
+        $id = $item->id;
+
+        // add the item properties
+        $storedItem = ['qty' => 0, 'price' => $item->price, 'item' => $item];
+
+        // check if the item exists in the cart before
+        // and if it exists then get it from the cart
+        if($this->items)
+        {
+            if(array_key_exists($id, $this->items))
+            {
+                $storedItem = $this->items[$id];
+            }
+        }
+
+        // update the cart increase qty and price
+        $storedItem['qty']++;
+        $storedItem['price'] = $item->price * $storedItem['qty'];
+        $this->items[$id] = $storedItem;
+        $this->totalQty++;
+        $this->totalPrice += $item->price;
+
+        // add the Cart to the Session
+        Session::put('cart', $this);
+
+        // return the Cart if needed
+        return $this;
     }
 }
