6dfb80c1313a93935a05bacadde20a73a1f93a1b
BahaaAlhagar <damrawyy@yahoo.com>
2017-12-31 10:30:30 +0200

    First commit - Package Sekeleton

---
+++ src/ShoppingCartServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BahaaAlhagar\ShoppingCart;
+
+use Illuminate\Support\ServiceProvider;
+
+class ShoppingCartServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
